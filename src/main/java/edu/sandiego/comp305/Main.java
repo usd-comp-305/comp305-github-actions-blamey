11dfdd4d623ec917e3c341fa8bc223980a23529b
Brett Lamey <blamey@sandiego.edu>
2026-04-25 20:55:14 -0700

    debug: added final to args parameter and added empty constructor to meet checkstyle requirements

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -3,7 +3,11 @@ package edu.sandiego.comp305;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+
+    private Main() {
+    }
+
+    public static void main(final String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
